02b61f88ec0b1849ef36c2ad1301e507e2693041
KristaaP <krista41319@gmail.com>
2024-08-20 13:56:34 -0400

    added search engine and dictionary components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import logo from "./logo.png";
+
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">hello</header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>Coded by SheCodes</small>
+        </footer>
+      </div>
     </div>
   );
 }
